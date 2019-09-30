57d51c6da664d5df87dc48f70b42f8d7d97af92a
zaknafein83 <franksisca@gmail.com>
2019-09-30 12:44:57 +0200

    security sul post

--- src/main/java/it/dstech/learning/library/controller/BookController.java
+++ src/main/java/it/dstech/learning/library/controller/BookController.java
@@ -3,6 +3,7 @@ package it.dstech.learning.library.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,8 @@ public class BookController {
 		return bookService.getLibri();
 	}
 
-	@ApiOperation(value = "Questo metodo di accesso libero mi da la lista di tutti i libri")
+	@ApiOperation(value = "Questo metodo permette ai soli admin di inserire libri")
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public Book saveBook(Book book) {
 		return bookService.aggiungiLibro(book);
